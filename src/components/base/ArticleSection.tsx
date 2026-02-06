91026542c28e18163cf107006334099ef4b1a7a6
encoderit-rafi <rafi@encoderit.net>
2026-02-06 17:04:09 +0600

    our articel

--- src/components/base/ArticleSection.tsx
+++ src/components/base/ArticleSection.tsx
@@ -119,7 +119,11 @@ const ArticleSection = ({ data }: TProps) => {
     //     </Slider>
     //   </div>
     // </div>
-    <div className={cn("")}>
+    <div
+      className={cn(
+        "relative overflow-hidden pl-4 pb-10 pt-4 sm:pb-15 lg:pb-18 lg:text-start"
+      )}
+    >
       {/* Section Header */}
       <div className="section flex items-center justify-between">
         <div>
@@ -132,61 +136,63 @@ const ArticleSection = ({ data }: TProps) => {
         </div>
       </div>
 
-      <div className="section">
-        {/* Scrollable Article Cards */}
-        <Slider
-          options={{
-            spaceBetween: 10,
-            slidesPerView: 1,
-            pagination: pagination,
-            navigation: true,
-            // autoplay: {
-            //   delay: 2500,
-            //   disableOnInteraction: false,
-            // },
-            breakpoints: {
-              320: {
-                slidesPerView: 1,
-                spaceBetween: 10,
+      <div>
+        <div className="section">
+          {/* Scrollable Article Cards */}
+          <Slider
+            options={{
+              spaceBetween: 10,
+              slidesPerView: 1,
+              pagination: pagination,
+              navigation: true,
+              // autoplay: {
+              //   delay: 2500,
+              //   disableOnInteraction: false,
+              // },
+              breakpoints: {
+                320: {
+                  slidesPerView: 1,
+                  spaceBetween: 10,
+                },
+                321: {
+                  slidesPerView: 1.2,
+                  spaceBetween: 10,
+                },
+                768: {
+                  slidesPerView: 2.5,
+                  spaceBetween: 20,
+                },
+                1024: {
+                  slidesPerView: 3,
+                  spaceBetween: 20,
+                },
+                1366: {
+                  slidesPerView: 3.5,
+                  spaceBetween: 20,
+                },
               },
-              321: {
-                slidesPerView: 1.2,
-                spaceBetween: 10,
-              },
-              768: {
-                slidesPerView: 2.5,
-                spaceBetween: 20,
-              },
-              1024: {
-                slidesPerView: 3,
-                spaceBetween: 20,
-              },
-              1366: {
-                slidesPerView: 3.5,
-                spaceBetween: 20,
-              },
-            },
-            // loop: true,
-          }}
-          sideOverlayClassName="bg-transparent"
-          className="overflow-visible topNavigation sm:pl-7 pt-4! pb-14! h-full"
-        >
-          {data.map(
-            (
-              { _id, cover_image, thumbnail_image, title, excerpt, slug },
-              index
-            ) => (
-              <SwiperSlide key={_id + index} className="h-auto! flex">
-                <ArticleCard
-                  image={thumbnail_image || cover_image}
-                  title={title}
-                  description={excerpt}
-                  slug={slug}
-                />
-              </SwiperSlide>
-            )
-          )}
-        </Slider>
+              // loop: true,
+            }}
+            sideOverlayClassName="bg-transparent"
+            className="overflow-visible topNavigation sm:pl-7 pt-4! pb-14! h-full"
+          >
+            {data.map(
+              (
+                { _id, cover_image, thumbnail_image, title, excerpt, slug },
+                index
+              ) => (
+                <SwiperSlide key={_id + index} className="h-auto! flex">
+                  <ArticleCard
+                    image={thumbnail_image || cover_image}
+                    title={title}
+                    description={excerpt}
+                    slug={slug}
+                  />
+                </SwiperSlide>
+              )
+            )}
+          </Slider>
+        </div>
       </div>
     </div>
   );
