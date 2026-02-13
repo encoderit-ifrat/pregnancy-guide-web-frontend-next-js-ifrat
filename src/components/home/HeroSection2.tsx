fd9303fe1de58f63212e56afa77f6d49d600b815
nahid <nahidferdous240@gmail.com>
2026-02-13 17:28:11 +0600

    Articles update

--- src/components/home/HeroSection2.tsx
+++ src/components/home/HeroSection2.tsx
@@ -16,7 +16,7 @@ export function HeroSection2({
   return (
     <section className="relative bg-[#F6F0FF] pt-2 pb-0 lg:pb-0">
       <div className="px-4 max-w-325 mx-auto w-full">
-        <div className="grid items-center gap-8 lg:grid-cols-[minmax(540px,1.3fr)_1fr] lg:gap-12">
+        <div className="grid items-center gap-8 lg:grid-cols-[minmax(600px,1.3fr)_1fr] lg:gap-12">
           {/* Content */}
           <div className="order-1 text-center lg:order-1 lg:text-left py-10">
             <p className="text-primary text-[20px] md:text-[25px] mb-4">{name}</p>
@@ -30,13 +30,12 @@ export function HeroSection2({
 
           {/* Tree Illustration */}
           <div className="order-2 flex min-h-[200px] justify-center lg:order-2 lg:justify-end">
-            {image && <div className="relative w-full max-w-md lg:max-w-lg max-h-[600px]">
+            {image && <div className="relative w-full max-w-md lg:max-w-lg h-auto min-h-[300px] sm:min-h-[400px] md:min-h-[500px]">
               <Image
                 src={imageLinkGenerator(image)}
                 alt={''}
-                width={500}
-                height={600}
-                className="max-h-[500px] w-full object-contain md:translate-y-5"
+                fill
+                className="object-contain md:translate-y-5"
                 priority
               />
             </div>}
