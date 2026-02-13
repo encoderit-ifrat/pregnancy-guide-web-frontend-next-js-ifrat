278ff83c3f5fad718d37e9d697f849f804db7f50
nahid <nahidferdous240@gmail.com>
2026-02-13 19:31:17 +0600

    po page

--- src/app/pregnancy-overview/_components/OverviewCategories.tsx
+++ src/app/pregnancy-overview/_components/OverviewCategories.tsx
@@ -112,7 +112,7 @@ function OverviewCategories() {
 
   return (
     <section className="w-full pt-10 max-w-6xl mx-auto">
-      <div className="flex items-center justify-center gap-8 md:gap-12 lg:gap-16 text-foreground px-4 md:px-6">
+      <div className="flex items-center justify-center gap-2 sm:gap-8 md:gap-12 lg:gap-16 text-foreground px-4 md:px-6">
         {overviewCategories.map((category) => {
           const { id, icon, name } = category;
           const isBaby = name === "Baby";
@@ -123,24 +123,55 @@ function OverviewCategories() {
               href={`/search-article?page=1&tag=${category.name}&week=${currentWeek}`}
               className="group"
             >
-              <div className="flex flex-col items-center cursor-pointer transition-transform hover:scale-105">
+              <div className="flex flex-col items-center cursor-pointer">
                 {/* Icon Container */}
                 <div className="relative">
-                  {/* Document Icon - positioned at top right */}
-                  <div className="absolute -top-2 -right-2 z-10">
-                    <IconPaper />
-                  </div>
-
-                  {/* Circular Container with Icon */}
                   <div
                     className={`
-                      relative rounded-full border-6 border-primary-light
-                      flex items-center justify-center
-                      ${isBaby ? 'w-32 h-32 md:w-48 md:h-48 lg:w-56 lg:h-56' : 'w-28 h-28 md:w-40 md:h-40 lg:w-48 lg:h-48'}
-                      transition-all group-hover:border-primary-light/80
-                    `}
+                    relative rounded-full border-6 border-primary-light
+                    flex items-center justify-center
+                    ${isBaby
+                        ? 'w-32 h-32 md:w-56 md:h-56 lg:w-68 lg:h-68'
+                        : 'w-28 h-28 md:w-36 md:h-36 lg:w-56 lg:h-56'}
+                    transition-all group-hover:border-primary-light/80
+                  `}
                   >
                     {icon}
+
+                    <div
+                      className="
+    absolute -top-2 -right-2 z-20
+    group-hover:top-1/2 
+    group-hover:left-1/2
+    group-hover:right-auto
+    group-hover:-translate-x-1/2
+    group-hover:-translate-y-1/2
+    rounded-full
+    group-hover:bg-primary-dark/40
+    group-hover:p-3
+    group-hover:size-42
+    transition-all duration-300 ease-in-out
+  "
+                    >
+                      <div className="flex flex-col items-center justify-center
+    rounded-full      
+    group-hover:bg-primary-dark
+    group-hover:p-6
+    group-hover:size-full
+    ">
+                        <IconPaper className="w-12 h-12 md:group-hover:w-12 md:group-hover:h-12 md:w-16 md:h-16 lg:w-20 lg:h-20" />
+                        <p className="hidden group-hover:block text-white! text-center text-xs pt-1 text-wrap">Read Article</p>
+                      </div>
+                    </div>
+
+                    {/* Paper Overlay */}
+                    {/* <div className="absolute inset-0 flex items-center justify-center
+                    opacity-0 scale-75
+                    transition-all duration-300
+                    group-hover:opacity-100 group-hover:scale-100
+                    z-20">
+                      <IconPaper className="w-16 h-16 md:w-24 md:h-24 lg:w-28 lg:h-28" />
+                    </div> */}
                   </div>
                 </div>
 
