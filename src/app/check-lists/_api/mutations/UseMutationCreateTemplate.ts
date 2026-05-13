9e8ccf742671bdb448d5af76ebbea8638106e6fe
encoderit-abrar <abrar@encoderit.net>
2026-05-13 17:11:44 +0600

    update checklist query

--- src/app/check-lists/_api/mutations/UseMutationCreateTemplate.ts
+++ src/app/check-lists/_api/mutations/UseMutationCreateTemplate.ts
@@ -1,12 +1,16 @@
 import api from "@/lib/axios";
-import { useMutation } from "@tanstack/react-query";
-
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export const useMutationCreateTemplate = () => {
-    return useMutation({
-        mutationFn: async (data: any) => {
-            const response = await api.post("/checklists/clone-from-templates", data);
-            return response.data;
-        },
-    });
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: async (data: any) => {
+      const response = await api.post("/checklists/clone-from-templates", data);
+      return response.data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["get-all-template"] });
+      queryClient.invalidateQueries({ queryKey: ["get-all-my-checklists"] });
+    },
+  });
 };
