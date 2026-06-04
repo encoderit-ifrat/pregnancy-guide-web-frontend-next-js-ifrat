d1a5317eee6ee79818c953f7a1366294a1789b04
nahid <nahidferdous240@gmail.com>
2026-06-04 14:42:59 +0600

    refactor: load `ViewReactTextEditor` dynamically with `ssr: false` in `BenefitContent` component

--- src/app/förmåner/[slug]/_components/BenefitContent.tsx
+++ src/app/förmåner/[slug]/_components/BenefitContent.tsx
@@ -1,6 +1,15 @@
 "use client";
 
-import { ViewReactTextEditor } from "@/app/pregnancy-overview/_components/ViewReactTextEditor";
+import React from "react";
+import dynamic from "next/dynamic";
+
+const ViewReactTextEditor = dynamic(
+  () =>
+    import("@/app/_pregnancy-overview/_components/ViewReactTextEditor").then(
+      (mod) => mod.ViewReactTextEditor,
+    ),
+  { ssr: false },
+);
 
 export default function BenefitContent({ content }: { content: string }) {
   return (
